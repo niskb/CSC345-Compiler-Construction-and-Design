292ac2c4e412fec3b2a96ef8ce4874a7a708932c
Brian Niski <50961607+niskb@users.noreply.github.com>
2024-08-28 08:41:39 -0400

    Define List of Reserved Words
    Initialize the reserved word list in the constructor.

--- Assignments/Assignment 1/Java Project/Niski-CSC345-Assignment-1/src/MyScanner.java
+++ Assignments/Assignment 1/Java Project/Niski-CSC345-Assignment-1/src/MyScanner.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyScanner {
 
-    enum TOKEN {
+    /**
+     * Tokens
+     */
+    private enum TOKEN {
         SCANEOF, ID, INTLITERAL, INTDATATYPE, DECLARE, PRINT, SET, EQUALS, IF, THEN, ENDIF, CALC, PLUS
+    }
+
+    /**
+     * Reserved Words
+     */
+    private List<String> RESERVED_WORDS = new ArrayList<String>();
+
+    /**
+     * Default Constructor for MyScanner
+     */
+    public MyScanner() {
+        /**
+         * Fill reserved words list
+         */
+        RESERVED_WORDS.add("declare");
+        RESERVED_WORDS.add("int");
+        RESERVED_WORDS.add("print");
+        RESERVED_WORDS.add("set");
+        RESERVED_WORDS.add("if");
+        RESERVED_WORDS.add("then");
+        RESERVED_WORDS.add("endif");
+        RESERVED_WORDS.add("calc");
     }
 
 }
