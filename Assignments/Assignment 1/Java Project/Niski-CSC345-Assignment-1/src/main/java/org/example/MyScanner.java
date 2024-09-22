29c927f87742bdfab71b3089c25d901e7721b3b7
Brian Niski <50961607+niskb@users.noreply.github.com>
2024-09-22 09:05:38 -0400

    Complete Homework Assignment 1
    Test Code works and everything is manually typed without any loops, but it's not that hard after figuring out you can nest a message inside the if statement.

--- Assignments/Assignment 1/Java Project/Niski-CSC345-Assignment-1/src/main/java/org/example/MyScanner.java
+++ Assignments/Assignment 1/Java Project/Niski-CSC345-Assignment-1/src/main/java/org/example/MyScanner.java
@@ -13,7 +13,7 @@ public class MyScanner {
     /**
      * Tokens
      */
-    private enum TOKEN {
+    public enum TOKEN {
         SCANEOF, ID, INTLITERAL, INTDATATYPE, DECLARE, PRINT, SET, EQUALS, IF, THEN, ENDIF, CALC, PLUS
     }
 
@@ -61,24 +61,204 @@ public class MyScanner {
      * @throws Exception
      */
     public TOKEN scan() throws Exception {
-        int read = INPUT.read();
-        // we can use a while loop and exit when -1
-
-        if (read != -1) { // return token if match found
-            char readChar = (char) read;
-            String readToken = "" + readChar;
-            // if statement needed for first char in token is a digit otherwise not INTLITERAL
-
-
-            // need to check next characters after then build token
-            // if read matches token, stop and return that token
-        } else { // no character has been read or empty program
-            INPUT.close();
+        int readChar = INPUT.read();
+        // Read until a whitespace is hit or end of file
+        String buffer = "";
+        while (readChar != -1) {
+            if (Character.isWhitespace(readChar)) {
+                break;
+            } else {
+                buffer = buffer + ((char) readChar);
+                readChar = INPUT.read();
+            }
+        }
+        // Expect a reserved word
+        if (TOKEN_BUFFER.toString().isEmpty()) { // The first reserved word
+            // Declare
+            switch (buffer) {
+                case "declare" -> {
+                    TOKEN_BUFFER.append(TOKEN.DECLARE);
+                    return TOKEN.DECLARE;
+                }
+                // Set
+                case "set" -> {
+                    TOKEN_BUFFER.append(TOKEN.SET);
+                    return TOKEN.SET;
+                }
+                // Calc
+                case "calc" -> {
+                    TOKEN_BUFFER.append(TOKEN.CALC);
+                    return TOKEN.CALC;
+                }
+                // Print
+                case "print" -> {
+                    TOKEN_BUFFER.append(TOKEN.PRINT);
+                    return TOKEN.PRINT;
+                }
+                // If
+                case "if" -> {
+                    TOKEN_BUFFER.append(TOKEN.IF);
+                    return TOKEN.IF;
+                }
+                // Empty program
+                case "" -> {
+                    TOKEN_BUFFER.append(TOKEN.SCANEOF);
+                    return TOKEN.SCANEOF;
+                }
+            }
+        } else {
+            // The second token to expect
+            // Declare
+            if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.DECLARE))) { // Expect an ID
+                TOKEN_BUFFER.append(TOKEN.ID);
+                return TOKEN.ID;
+                // Set
+            } else if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.SET))) { // Expect an ID
+                TOKEN_BUFFER.append(TOKEN.ID);
+                return TOKEN.ID;
+                // Calc
+            } else if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.CALC))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+                // Print
+            } else if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.PRINT))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+                // If
+            } else if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+            }
+            // The third token to expect
+            // Declare
+            if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.DECLARE) + String.valueOf(TOKEN.ID))) { // Expect an Int Datatype
+                TOKEN_BUFFER.append(TOKEN.INTDATATYPE);
+                return TOKEN.INTDATATYPE;
+                // Set
+            } else if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.SET) + String.valueOf(TOKEN.ID))) { // Expect an Equal Sign
+                TOKEN_BUFFER.append(TOKEN.EQUALS);
+                return TOKEN.EQUALS;
+                // Calc
+            } else if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.CALC) + String.valueOf(TOKEN.INTLITERAL))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.CALC) + String.valueOf(TOKEN.ID)))) { // Expect a Plus Sign
+                TOKEN_BUFFER.append(TOKEN.PLUS);
+                return TOKEN.PLUS;
+                // If
+            } else if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID)))) {
+                TOKEN_BUFFER.append(TOKEN.EQUALS);
+                return TOKEN.EQUALS;
+            }
+            // The fourth token to expect
+            // Set
+            if (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.SET) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+                // Calc
+            } else if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.CALC) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.PLUS))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.CALC) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.PLUS)))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+                // If
+            } else if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.EQUALS))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS)))) { // Expect an ID or Int Literal
+                try { // Int Literal
+                    int test = Integer.parseInt(buffer);
+                    TOKEN_BUFFER.append(TOKEN.INTLITERAL);
+                    return TOKEN.INTLITERAL;
+                } catch (Exception e) { // ID
+                    TOKEN_BUFFER.append(TOKEN.ID);
+                    return TOKEN.ID;
+                }
+            }
+            // The fifth token to expect
+            // If
+            if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.INTLITERAL))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.ID))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.ID))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.INTLITERAL)))) {
+                TOKEN_BUFFER.append(TOKEN.THEN);
+                return TOKEN.THEN;
+            }
+            // The sixth token to expect
+            // If, it's either ENDIF or PRINT
+            if ((TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.THEN)) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.THEN))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.THEN))) || (TOKEN_BUFFER.toString().equals(String.valueOf(TOKEN.IF) + String.valueOf(TOKEN.ID) + String.valueOf(TOKEN.EQUALS) + String.valueOf(TOKEN.INTLITERAL) + String.valueOf(TOKEN.THEN))))) {
+                // We are now in the if statement, use the PushbackReader until we hit "endif" so we can exit!
+                String innerBuffer = buffer;
+                int innerReadChar = INPUT.read();
+                while (innerReadChar != -1) {
+                    innerBuffer = innerBuffer + ((char) innerReadChar);
+                    innerReadChar = INPUT.read();
+                }
+                // Process the inner part of the if statement and get rid of ' ' whitespaces
+                innerBuffer = innerBuffer.trim();
+                innerBuffer = innerBuffer.substring(0, innerBuffer.length() - 5);
+                innerBuffer = innerBuffer.toUpperCase();
+                // Parse the command
+                innerBuffer = parseInnerBuffer(innerBuffer);
+                // Add parsed command to the Token_Buffer
+                TOKEN_BUFFER.append(innerBuffer);
+                // The last step
+                TOKEN_BUFFER.append(TOKEN.ENDIF);
+                return TOKEN.ENDIF;
+            }
         }
         return TOKEN.SCANEOF;
     }
 
-    // This method is not tested yet
+    /**
+     * Method that takes a String buffer and takes its lexemes and turns it into a group of tokens and return as a String
+     *
+     * @param innerBuffer
+     * @return
+     */
+    private String parseInnerBuffer(String innerBuffer) {
+        String parsedString = innerBuffer;
+        // Declare ...
+
+        // Set ...
+
+        // Calc ...
+
+        // Print <- This is the only required function on the homework assignment
+        if (innerBuffer.contains(String.valueOf(TOKEN.PRINT))) {
+            parsedString = parsedString.substring(parsedString.lastIndexOf(String.valueOf(TOKEN.PRINT)) + String.valueOf(TOKEN.PRINT).length() + 1).trim();
+            // The rest is the ID or Int Literal
+            try { // Int Literal
+                int test = Integer.parseInt(parsedString);
+                parsedString = "" + TOKEN.PRINT + test;
+            } catch (Exception e) { // ID
+                parsedString = "" + TOKEN.PRINT + TOKEN.ID;
+            }
+        }
+        // If ...
+
+        // Empty ...
+        return parsedString;
+    }
 
     /**
      * Method to get the token buffer string
---
+++ Assignments/Assignment 1/Java Project/Niski-CSC345-Assignment-1/src/test/java/org/example/MyScannerTest.java
@@ -0,0 +1,308 @@
+/**
+ * Brian Niski
+ */
+
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.PushbackReader;
+import java.io.StringReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyScannerTest {
+
+    @Test
+    void scanFirst() {
+        // Assign
+        String testString = "declare x int";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.DECLARE), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.DECLARE + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Third Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Third Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.DECLARE + MyScanner.TOKEN.ID + MyScanner.TOKEN.INTDATATYPE), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanSecond() {
+        // Assign
+        String testString = "set x = 5";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.SET), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.SET + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Third Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Third Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.SET + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fourth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fourth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.SET + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.INTLITERAL), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanThird() {
+        // Assign
+        String testString = "calc x + y";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.CALC), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.CALC + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Third Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Third Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.CALC + MyScanner.TOKEN.ID + MyScanner.TOKEN.PLUS), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fourth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fourth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.CALC + MyScanner.TOKEN.ID + MyScanner.TOKEN.PLUS + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanFourth() {
+        // Assign
+        String testString = "print x";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.PRINT), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.PRINT + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanFifth() {
+        // Assign
+        String testString = "if x = y then \n endif";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Third Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Third Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fourth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fourth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fifth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fifth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID + MyScanner.TOKEN.THEN), String.valueOf(myScanner.getTokenBufferString()));
+
+        /**
+         *  We are in the if statement, so the code "jumps" into another set of curly braces.
+         */
+
+        // Sixth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Sixth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID + MyScanner.TOKEN.THEN + MyScanner.TOKEN.ENDIF), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanSixth() {
+        // Assign
+        String testString = "if x = y then \n print x \n endif";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // First Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // First Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Second Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Second Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Third Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Third Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fourth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fourth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID), String.valueOf(myScanner.getTokenBufferString()));
+
+        // Fifth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Fifth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID + MyScanner.TOKEN.THEN), String.valueOf(myScanner.getTokenBufferString()));
+
+        /**
+         *  We are in the if statement, so the code "jumps" into another set of curly braces.
+         */
+
+        // Sixth Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // Sixth Assert
+        assertEquals(String.valueOf("" + MyScanner.TOKEN.IF + MyScanner.TOKEN.ID + MyScanner.TOKEN.EQUALS + MyScanner.TOKEN.ID + MyScanner.TOKEN.THEN + MyScanner.TOKEN.PRINT + MyScanner.TOKEN.ID + MyScanner.TOKEN.ENDIF), String.valueOf(myScanner.getTokenBufferString()));
+    }
+
+    @Test
+    void scanSeventh() {
+        // Assign
+        String testString = "";
+        MyScanner myScanner = new MyScanner(new PushbackReader(new StringReader(testString)));
+
+        // Act
+        try {
+            myScanner.scan();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Assert
+        assertEquals(String.valueOf(MyScanner.TOKEN.SCANEOF), String.valueOf(myScanner.getTokenBufferString()));
+
+    }
+}
