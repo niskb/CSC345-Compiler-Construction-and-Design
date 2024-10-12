cb369ff36cf6e366ae36bda99ae37ffd147466de
Brian Niski <50961607+niskb@users.noreply.github.com>
2024-10-11 23:16:57 -0400

    Main Class
    Run parser through input file!

--- Assignments/Assignment 2/Java Project/Niski-CSC345-Assignment-2/src/main/java/niski/csc/Main.java
+++ Assignments/Assignment 2/Java Project/Niski-CSC345-Assignment-2/src/main/java/niski/csc/Main.java
@@ -1,17 +1,23 @@
+/**
+ * Brian Niski
+ */
+
 package niski.csc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        String program = "";
+        try {
+            program = Files.readString(Path.of("input.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        boolean parse = new MyParser().parse(program);
+        System.out.println("Result: " + parse);
     }
 }
